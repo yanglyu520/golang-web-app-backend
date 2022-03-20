7fb67e573601380427be233a046e65408c19494a
Yang Lyu <42168739+yanglyu520@users.noreply.github.com>
2022-03-21 00:06:42 +1100

    Create bufio-scanner-main.go

---
+++ bufio-scanner-main.go
@@ -0,0 +1,24 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "sss dfdfa dsf"
+	//strings.NewReader will take a string and return a pointer to a reader
+	scanner := bufio.NewScanner(strings.NewReader(s))
+
+	//Scanner provides a convenient interface for reading data such as a file of newline-delimited lines of text. Successive calls to the Scan method will step through the 'tokens' of a file, skipping the bytes between the tokens. The specification of a token is defined by a split function of type SplitFunc;
+	scanner.Split(bufio.ScanWords)
+	scanner.Split(bufio.ScanRunes)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+}
